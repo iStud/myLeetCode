dede51199e4d04f5a800f2e1ed90d59994a32a79
xiongbk <istud.k@gmail.com>
2021-11-23 18:29:30 +0800

    链表相交

--- 代码随想录/链表_160_链表相交.cpp
+++ 代码随想录/链表_160_链表相交.cpp
@@ -12,40 +12,6 @@ struct ListNode
     ListNode *next;
 };
 
-// 创建一个链表
-ListNode *create()
-{
-    ListNode *head = new ListNode; // 创建了一个头节点
-    ListNode *p = head;            // p 指针指向了这个节点
-
-    int x, cycle = 1;
-    while (cycle)
-    {
-        cout << "请输入链表数据,输入0结束输入" << endl;
-        cin >> x;
-
-        if (x != 0)
-        {
-
-            ListNode *node = new ListNode; // 循环创建新的节点并给新的节点赋值
-            node->val = x;
-
-            p->next = node;
-            p = node;
-        }
-        else
-        {
-            cycle = 0;
-            cout << "输入完成" << endl;
-        }
-    }
-
-    head = head->next;
-    p->next = NULL;
-
-    return head;
-}
-
 // 打印链表
 void printListNode(ListNode *node)
 {
@@ -75,32 +41,42 @@ ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
 
 int main()
 {
+    // 相交的部分
     ListNode *c = new ListNode;
     c->val = 3;
     c->next = new ListNode;
     c->next->val = 4;
+    c->next->next = new ListNode;
+    c->next->next->val = 7;
 
-    
-
+    // 链表a
     ListNode *a = new ListNode;
     a->val = 1;
 
     a->next = new ListNode;
     a->next->val = 4;
 
-    a->next->next = c;
+    a->next->next = new ListNode;
+    a->next->next->val = 5;
+
+    a->next->next->next = new ListNode;
+    a->next->next->next->val = 6;
+
+    a->next->next->next->next = c;
+
     cout << "链表 a" << endl;
     printListNode(a);
 
+    // 链表 b
     ListNode *b = new ListNode;
     b->val = 2;
     b->next = c;
+
     cout << "链表 b" << endl;
     printListNode(b);
- 
-    cout << "相交后" << endl;
-    printListNode(getIntersectionNode(a,b));
 
+    cout << "相交后的链表" << endl;
+    printListNode(getIntersectionNode(a, b));
 
     system("pause");
     return 0;

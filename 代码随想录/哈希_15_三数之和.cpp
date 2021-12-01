3eda42ecf1dd8e65e6a73d818b38469d6186a608
xiongbk <istud.k@gmail.com>
2021-12-01 14:58:12 +0800

    提交

--- 代码随想录/哈希_15_三数之和.cpp
+++ 代码随想录/哈希_15_三数之和.cpp
@@ -8,8 +8,8 @@ vector<vector<int> >  threeSum(vector<int> &nums)
 {
     int size = nums.size();
     if (size < 3)
-        return {};                       // 特判
-    vector<vector<int> > res;            // 保存结果（所有不重复的三元组）
+        return {};                       
+    vector<vector<int> > res;            
     std::sort(nums.begin(), nums.end()); // 排序（默认递增）
     for (int i = 0; i < size; i++)       // 固定第一个数，转化为求两数之和
     {
@@ -17,8 +17,9 @@ vector<vector<int> >  threeSum(vector<int> &nums)
             return res; // 第一个数大于 0，后面都是递增正数，不可能相加为零了
 
         // 去重：如果此数已经选取过，跳过
-        // if (i > 0 && nums[i] == nums[i - 1])
-        //     continue;
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
         // 双指针在nums[i]后面的区间中寻找和为0-nums[i]的另外两个数
         int left = i + 1;
         int right = size - 1;
@@ -37,14 +38,15 @@ vector<vector<int> >  threeSum(vector<int> &nums)
                 v.push_back(nums[left]);
                 v.push_back(nums[right]);
                 res.push_back(v);
-                // res.push_back(vector<int>{nums[i], nums[left], nums[right]});
+               
                 left++;
                 right--;
+                
                 // 去重：第二个数和第三个数也不重复选取
-                // while (left < right && nums[left] == nums[left - 1])
-                //     left++;
-                // while (left < right && nums[right] == nums[right + 1])
-                //     right--;
+                while (left < right && nums[left] == nums[left - 1])
+                    left++;
+                while (left < right && nums[right] == nums[right + 1])
+                    right--;
             }
         }
     }

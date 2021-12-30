2a3c51ec21cf914ad72ff907767d75c53e0be5cb
xiongbk <istud.k@gmail.com>
2021-12-30 18:33:28 +0800

    二叉树

---
+++ 代码随想录/二叉树_501_二叉搜索树中的众数.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+};
+
+int cot;
+int maxCount;
+int base;
+vector<int> result;
+
+void updateValue(int value)
+{
+
+    if (base == value)
+    {
+        cot++;
+    }
+    else
+    {
+        base = value;
+        cot = 1;
+    }
+
+    if (cot == maxCount)
+    {
+        result.push_back(base);
+    }
+
+    if (cot > maxCount)
+    {
+        maxCount = cot;
+        result = vector<int>{base};
+    }
+}
+
+void searchBST(TreeNode *root)
+{
+    if (root == nullptr)
+        return;
+
+    searchBST(root->left);
+    updateValue(root->val);
+    searchBST(root->right);
+}
+
+int main()
+{
+    TreeNode *root = new TreeNode;
+    root->val = 1;
+
+    TreeNode *node1 = new TreeNode;
+    node1->val = 2;
+
+    TreeNode *node2 = new TreeNode;
+    node2->val = 2;
+
+    root->right = node1;
+    node1->right = node2;
+
+    searchBST(root);
+
+    system("pause");
+    return 0;
+}
